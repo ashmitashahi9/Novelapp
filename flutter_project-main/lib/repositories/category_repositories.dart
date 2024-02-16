1bb485d5589696a4ed21b822752ff890305bea95
ashmitashahi9 <ashmitashahi67@gmail.com>
2024-02-16 09:39:23 +0545

    first commit

---
+++ flutter_project-main/lib/repositories/category_repositories.dart
@@ -0,0 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../models/category_model.dart';
+import '../models/user_model.dart';
+import '../services/firebase_service.dart';
+
+class CategoryRepository{
+  CollectionReference<CategoryModel> categoryRef = FirebaseService.db.collection("categories")
+      .withConverter<CategoryModel>(
+    fromFirestore: (snapshot, _) {
+      return CategoryModel.fromFirebaseSnapshot(snapshot);
+    },
+    toFirestore: (model, _) => model.toJson(),
+  );
+    Future<List<QueryDocumentSnapshot<CategoryModel>>> getCategories() async {
+    try {
+      var data = await categoryRef.get();
+      bool hasData = data.docs.isNotEmpty;
+      if(!hasData){
+        makeCategory().forEach((element) async {
+          await categoryRef.add(element);
+        });
+      }
+      final response = await categoryRef.get();
+      var category = response.docs;
+      return category;
+    } catch (err) {
+      print(err);
+      rethrow;
+    }
+  }
+
+  Future<DocumentSnapshot<CategoryModel>>  getCategory(String categoryId) async {
+      try{
+        print(categoryId);
+        final response = await categoryRef.doc(categoryId).get();
+        return response;
+      }catch(e){
+        rethrow;
+      }
+  }
+
+  List<CategoryModel> makeCategory(){
+      return [
+        CategoryModel(categoryName: "Action Novel", status: "active", imageUrl: "https://images.unsplash.com/photo-1591117207239-788bf8de6c3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80"),
+        CategoryModel(categoryName: "Fantasy Novel", status: "active", imageUrl: "https://i.pinimg.com/564x/fd/ac/47/fdac471a7a0efb4b875479a92b4ec506.jpg"),
+        CategoryModel(categoryName: "Romance Novel", status: "active", imageUrl: "https://images.unsplash.com/photo-1492175742197-ed20dc5a6bed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"),
+        CategoryModel(categoryName: "Adventure Novel", status: "active", imageUrl: "https://images.unsplash.com/photo-1547469447-4afec158715b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"),
+        CategoryModel(categoryName: "Sci-Fi Novel", status: "active", imageUrl: "https://images.unsplash.com/photo-1547469447-4afec158715b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"),
+      ];
+  }
+
+
+
+}
